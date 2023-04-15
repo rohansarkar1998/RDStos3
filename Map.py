ce3ed54b21aa7915b9826f2b80ec451eeefcdcc8
rohan1998 <rohansarkarmsit@gmail.com>
2023-04-16 01:38:40 +0530

    remote push

---
+++ Map.py
@@ -0,0 +1,35 @@
+import csv
+
+import pymysql
+
+
+class City:
+    def __init__(self, ID, Name, CountryCode, District, Population):
+        self.ID = ID
+        self.Name = Name
+        self.CountryCode = CountryCode
+        self.District = District
+        self.Population = Population
+
+
+Citydetails = []
+dbHost = "192.168.0.5"
+dbUser = "app"
+dbPass = "password"
+dbName = "world"
+mydb = pymysql.connect(host=dbHost, user=dbUser, password=dbPass, database=dbName)
+mycorsor = mydb.cursor()
+mycorsor.execute("select * from city")
+name = mycorsor.fetchall()
+for i in name:
+    print(i)
+
+
+field = ['Id', 'Name', 'Countrycode', 'Dist', 'Population']
+filename = 'D:\AWS\City.csv'
+with open(filename, 'w', encoding='utf8', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(field)
+    for i in name:
+        csvwriter.writerow(i)
+
